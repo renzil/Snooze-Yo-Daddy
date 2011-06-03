f0f998b1f7f64ace0991220988f6a77a43cf53ac
Renzil DSouza <renzil@gmail.com>
2011-06-03 19:20:28 +0530

    Checking in basic Android project.

---
+++ src/com/syd/activity/SYDLaunchActivity.java
@@ -0,0 +1,15 @@
+package com.syd.activity;
+
+import com.syd.*;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SYDLaunchActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
